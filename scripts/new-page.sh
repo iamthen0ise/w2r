3ac0681c2c8afa2aef3d84282a5718d2a8fcdc3e
iamthen0ise <thefiltdj@gmail.com>
2021-01-17 14:08:37 +0300

    Test actions 2

---
+++ scripts/new-page.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DATE=$(date "+%Y-%m-%dT%H:%M:%S%z")
+TITLE=$(echo $1)
+FILENAME=$(echo "$TITLE.md" | tr " " "-" | tr '[:upper:]' '[:lower:]')
+hugo new items/$FILENAME
+cat > items/$FILENAME <<EOF
+---
+title: "$TITLE"
+date: $DATE
+itemurl: "$2"
+sites: "$(echo $(echo "$2" | awk -F/ '{print $3}'))"
+tags: $3
+draft: false
+---
+EOF
