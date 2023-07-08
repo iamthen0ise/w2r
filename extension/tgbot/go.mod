9b642ef1464b645a6e09e35557e4ef5501cd740c
evgenii.uvarov <evgenii.uvarov.post@gmail.com>
2023-07-08 13:47:39 +0300

    Refactor

--- extension/tgbot/go.mod
+++ extension/tgbot/go.mod
@@ -2,4 +2,13 @@ module github.com/iamthen0ise/w2r-tgbot
 
 go 1.18
 
-require golang.org/x/net v0.12.0
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/net v0.7.0 // indirect
+)
--- extension/tgbot/main.go
+++ extension/tgbot/main.go
@@ -8,238 +8,223 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
+	"regexp"
 	"strings"
-	"syscall"
 	"time"
 
-	"golang.org/x/net/html"
-)
-
-const (
-	telegramAPIBaseURL = "https://api.telegram.org/bot"
-	maxRetries         = 5
+	"github.com/PuerkitoBio/goquery"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 var (
-	botToken     string
-	githubToken  string
-	shutdown     chan os.Signal
-	httpClient   *http.Client
-	githubApiUrl string
+	urlPattern     = regexp.MustCompile(`(http|https)://[^\s]+`)
+	hashtagPattern = regexp.MustCompile(`#[^\s]+`)
 )
 
-type Update struct {
-	ID          int
-	Message     *struct{ Text string }
-	ChannelPost *struct{ Text string }
+// Struct to hold URL information
+type URLInfo struct {
+	URL      string
+	Title    string
+	Hashtags string
 }
 
 func main() {
-	botToken = os.Getenv("TELEGRAM_BOT_TOKEN")
-	githubToken = os.Getenv("GITHUB_TOKEN")
-	githubApiUrl = os.Getenv("GITHUB_API_URL")
-
-	if botToken == "" || githubToken == "" || githubApiUrl == "" {
-		log.Fatal("Please provide required environment variables: TELEGRAM_BOT_TOKEN, API_URL, GITHUB_TOKEN, GITHUB_REPOSITORY")
+	// Retrieve the Telegram Bot Token from environment variable
+	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	if botToken == "" {
+		log.Fatal("TELEGRAM_BOT_TOKEN environment variable not set")
 	}
 
-	httpClient = configureHTTPClient(time.Second * 10)
-
-	shutdown = make(chan os.Signal, 1)
-	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
-
-	go pollUpdates()
-
-	<-shutdown
-	log.Println("Shutting down...")
-}
-
-func configureHTTPClient(timeout time.Duration) *http.Client {
-	return &http.Client{
-		Timeout: timeout,
+	// Retrieve the API Token from environment variable
+	apiToken := os.Getenv("GITHUB_TOKEN")
+	if apiToken == "" {
+		log.Fatal("GITHUB_TOKEN environment variable not set")
 	}
-}
 
-func pollUpdates() {
-	offset := 0
+	// Retrieve the Workflow URL from environment variable
+	workflowURL := os.Getenv("GITHUB_API_URL")
+	if workflowURL == "" {
+		log.Fatal("GITHUB_API_URL environment variable not set")
+	}
 
-	for {
-		select {
-		case <-shutdown:
-			return
-		default:
-			updates, err := getUpdates(offset)
+	// Create a new bot instance
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Fatalf("Failed to create new Bot API: %v", err)
+	}
+
+	// Enable debug mode
+	bot.Debug = true
+
+	// Create a new update configuration
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 60
+
+	// Get updates from the bot
+	updates, err := bot.GetUpdatesChan(updateConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Process incoming updates
+	for update := range updates {
+		if update.Message == nil {
+			log.Println("Received nil Message from the update")
+			continue
+		}
+
+		// Get the message text
+		messageText := update.Message.Text
+
+		// Check if the message is a forward from another chat, bot, or channel
+		if update.Message.ForwardFrom != nil || update.Message.ForwardFromChat != nil {
+			messageText = fmt.Sprintf("Forwarded message: %s", messageText)
+		}
+
+		// Find URLs in the message
+		urls := urlPattern.FindAllString(messageText, -1)
+
+		// Find hashtags in the message
+		hashtags := concatenateHashtags(hashtagPattern.FindAllString(messageText, -1))
+
+		// Process each URL
+		for _, url := range urls {
+			// Grab the title of the URL
+			title, err := getTitleFromURL(url)
 			if err != nil {
-				log.Println("Failed to get updates:", err)
-				retryPollUpdates(&offset)
+				log.Printf("Error getting title for URL %s: %v\n", url, err)
+				continue
+			}
+
+			// Create URLInfo struct
+			urlInfo := URLInfo{
+				URL:      url,
+				Title:    title,
+				Hashtags: hashtags,
+			}
+
+			// Log URLInfo
+			fmt.Printf("URL: %s, Title: %s, Hashtags: %v\n", urlInfo.URL, urlInfo.Title, urlInfo.Hashtags)
+
+			// Trigger GitHub Actions workflow
+			err = triggerWorkflowRun(apiToken, workflowURL, urlInfo)
+			if err != nil {
+				log.Printf("Error triggering workflow: %v\n", err)
+				// Send a message to the user indicating the error
+				sendMessage(bot, update.Message.Chat.ID, fmt.Sprintf("Failed to send URL '%s' to GitHub", urlInfo.URL))
 			} else {
-				for _, update := range updates {
-					offset = update.ID + 1
-
-					var text string
-					if update.Message != nil {
-						text = update.Message.Text
-					} else if update.ChannelPost != nil {
-						text = update.ChannelPost.Text
-					}
-
-					url, tags := parseURLAndTags(text)
-					title, _ := getTitle(url)
-
-					if url != "" {
-						err := sendPOSTRequest(url, tags, title)
-						if err != nil {
-							log.Println("Error sending POST request:", err)
-						} else {
-							log.Println("POST request sent successfully!")
-						}
-					}
-				}
+				// Send a message to the user indicating success
+				sendMessage(bot, update.Message.Chat.ID, fmt.Sprintf("URL '%s' sent to GitHub successfully", urlInfo.URL))
 			}
-			time.Sleep(time.Second)
 		}
 	}
 }
 
-func retryPollUpdates(offset *int) {
-	for i := 0; i < maxRetries; i++ {
-		sleepDuration := time.Duration(1<<i) * time.Second
-		log.Printf("Retrying after %s...", sleepDuration)
-		time.Sleep(sleepDuration)
-
-		_, err := getUpdates(*offset)
-		if err == nil {
-			return
-		}
-	}
-
-	log.Printf("Exceeded maximum retries. Exiting...")
-	os.Exit(1)
-}
-
-func getUpdates(offset int) ([]Update, error) {
-	url := fmt.Sprintf("%s%s/getUpdates?offset=%d", telegramAPIBaseURL, botToken, offset)
-
-	resp, err := httpClient.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	var response struct {
-		Result []Update `json:"result"`
-	}
-
-	err = json.NewDecoder(resp.Body).Decode(&response)
-	if err != nil {
-		return nil, err
-	}
-
-	return response.Result, nil
-}
-
-func getTitle(url string) (string, error) {
-	// Send a GET request to the URL
-	resp, err := http.Get(url)
+// getTitleFromURL retrieves the title of a webpage given its URL
+func getTitleFromURL(url string) (string, error) {
+	// Make an HTTP GET request to the URL
+	response, err := http.Get(url)
 	if err != nil {
 		return "", err
 	}
-	defer resp.Body.Close()
+	defer response.Body.Close()
 
-	// Parse the HTML response
-	doc, err := html.Parse(resp.Body)
+	// Use goquery to parse the HTML response
+	doc, err := goquery.NewDocumentFromReader(response.Body)
 	if err != nil {
 		return "", err
 	}
 
-	// Traverse the HTML tree and find the title tag
-	var f func(*html.Node) string
-	f = func(n *html.Node) string {
-		if n.Type == html.ElementNode && n.Data == "title" {
-			return n.FirstChild.Data
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			title := f(c)
-			if title != "" {
-				return title
-			}
-		}
-		return ""
+	// Extract the title tag value
+	title := doc.Find("title").First().Text()
+
+	// Clean up the title by removing leading/trailing whitespace and newlines
+	title = strings.TrimSpace(title)
+
+	return title, nil
+}
+
+// triggerWorkflowRun triggers a GitHub Actions workflow run with the provided payload
+func triggerWorkflowRun(apiToken string, workflowURL string, payload URLInfo) error {
+	// Create the request payload
+	type RequestPayload struct {
+		EventType     string `json:"event_type"`
+		ClientPayload struct {
+			URL   string `json:"url"`
+			Title string `json:"title"`
+			Tags  string `json:"tags"`
+		} `json:"client_payload"`
 	}
 
-	return strings.TrimSpace(f(doc)), nil
-}
-
-func parseURLAndTags(text string) (string, []string) {
-	var url string
-	var tags []string
-
-	words := strings.Fields(text)
-
-	for _, word := range words {
-		if strings.HasPrefix(word, "#") {
-			// Remove the leading "#" from the tag
-			tag := strings.TrimPrefix(word, "#")
-			tags = append(tags, tag)
-		} else if strings.HasPrefix(word, "http://") || strings.HasPrefix(word, "https://") {
-			url = word
-			break
-		}
-	}
-
-	return url, tags
-}
-
-type Dispatch struct {
-	EventType     string        `json:"event_type"`
-	ClientPayload ClientPayload `json:"client_payload"`
-}
-
-type ClientPayload struct {
-	URL   string   `json:"url"`
-	Title string   `json:"title"`
-	Tags  []string `json:"tags"`
-}
-
-func sendPOSTRequest(url string, tags []string, title string) error {
-	data := Dispatch{
+	requestPayload := RequestPayload{
 		EventType: "webhook",
-		ClientPayload: ClientPayload{
-			URL:   url,
-			Title: title,
-			Tags:  tags,
-		},
+		ClientPayload: struct {
+			URL   string "json:\"url\""
+			Title string "json:\"title\""
+			Tags  string "json:\"tags\""
+		}{},
 	}
 
-	payloadBytes, err := json.Marshal(data)
+	// Convert the payload to JSON
+	jsonPayload, err := json.Marshal(requestPayload)
 	if err != nil {
-		fmt.Printf("Error: %s", err)
 		return err
 	}
 
-	// Create the request
-	req, err := http.NewRequest("POST", githubApiUrl, bytes.NewReader(payloadBytes))
+	// Create the HTTP request
+	client := &http.Client{Timeout: 10 * time.Second}
+	request, err := http.NewRequest(http.MethodPost, workflowURL, bytes.NewBuffer(jsonPayload))
 	if err != nil {
-		fmt.Printf("Error: %s", err)
 		return err
 	}
 
-	// Add the necessary headers
-	req.Header.Add("Accept", "application/vnd.github.v3+json")
-	req.Header.Add("Authorization", githubToken)
-	req.Header.Add("Content-Type", "application/json")
+	// Set the necessary headers for authentication and content type
+	request.Header.Set("Accept", "application/vnd.github.v3+json")
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiToken))
+
+	// Log the request information
+	log.Printf("GitHub Request URL: %s", request.URL.String())
+	log.Printf("GitHub Request Method: %s", request.Method)
+	log.Printf("GitHub Request Body: %s", jsonPayload)
 
 	// Send the request
-	resp, err := httpClient.Do(req)
+	response, err := client.Do(request)
 	if err != nil {
-		fmt.Printf("Error: %s", err)
 		return err
 	}
-	defer resp.Body.Close()
+	defer response.Body.Close()
 
-	// Print the response
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Printf("Response: %s", body)
+	// Log the response information
+	log.Printf("GitHub Response Status: %s", response.Status)
+	log.Printf("GitHub Response Headers: %v", response.Header)
+	// Read the response body
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	log.Printf("GitHub Response Body: %s", responseBody)
+
+	// Check the response status code
+	if response.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("workflow run failed with status code: %d", response.StatusCode)
+	}
+
 	return nil
+}
+
+// sendMessage sends a message to the specified chat ID using the Telegram bot
+func sendMessage(bot *tgbotapi.BotAPI, chatID int64, message string) {
+	msg := tgbotapi.NewMessage(chatID, message)
+	_, err := bot.Send(msg)
+	if err != nil {
+		log.Printf("Error sending message: %v\n", err)
+	}
+}
+
+func concatenateHashtags(hashtags []string) string {
+	if len(hashtags) > 0 {
+		return strings.Join(hashtags, ",")
+	}
+	return "unsorted"
 }
